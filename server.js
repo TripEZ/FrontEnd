60d56a9e9857760c912d432300265c2d7dd5e034
Gurwinder Singh <sainirebel@gmail.com>
2022-03-20 20:05:17 -0400

    Two Factor Authentication

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
+const nunjucks = require('nunjucks')
+const Nexmo = require('nexmo')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+nunjucks.configure('dist/frontend/', {express:app})
+
+const nexmo = new Nexmo({ 
+    apiKey: '48441f95',
+    apiSecret: 'pyXFXNrJhdPFc6rO'
+  })
+  
+  app.get('/', (req, res) => { 
+    res.render('twoFactor.html', { message: 'Enter your verification code' }) 
+  })
+
+  app.post('/verify', (req, res) => {
+    nexmo.verify.request({
+      number: req.body.number,
+      brand: 'ACME Corp'
+    }, (error, result) => {
+      if(result.status != 0) {
+        res.render('twoFactor.html', { message: result.error_text })
+      } else {
+        res.render('check.html', { requestId: result.request_id })
+      }
+    })
+    app.post('/check', (req, res) => {
+        nexmo.verify.check({
+          request_id: req.body.requestId,
+          code: req.body.code
+        }, (error, result) => {
+          if(result.status != 0) {
+            res.render('twoFactor.html', { message: result.error_text })
+          } else {
+            res.render('success.html')
+          }
+        })
+      })
+  })
 const app = express();
 
 // Serve only the static files form the dist directory
