2c5415ef94abc036862d579e0893066f1a10b904
Carlos-Mac <stayputman@gmail.com>
2022-04-17 00:34:29 -0400

    Added G. Analytics API as per Iteration 4 -Carlos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { title } from 'process';
+import { filter } from 'rxjs/operators';
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
+  
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title ='frontend';
+  constructor(private router: Router) {
+    
+  }
+
+  ngOnInit() {
+      this.setUpAnalytics();
+  }
+
+  //Setup analytics for any event
+  setUpAnalytics() {
+      this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+          .subscribe((event: any) => {
+              gtag('config', 'G-YOUR-GOOGLE-ID',
+                  {
+                      page_path: event.urlAfterRedirects
+                  }
+              );
+          });
+  }
 }
