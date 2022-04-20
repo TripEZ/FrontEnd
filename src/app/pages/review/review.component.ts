8554387b1f31c51a573525b858f14d6a3000967f
Julian Miguel Alapan <jalapan1@my.centennialcollege.ca>
2022-04-20 09:36:03 -0400

    update review

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -1,11 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavigationEnd, NavigationStart, RouterEvent } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { Observable } from 'rxjs';
-import {map} from "rxjs/operators";
+import { Subscription } from 'rxjs';
+
+interface reviewResponse{
+  getReview : {
+    reviews : reviews[]
+  };
+}
+
+interface inputReview{
+  userId:any;
+  reviewName:string;
+  reviewEmail:string;
+  reviewCountry:string;
+  reviewText:string;
+}
+
+interface reviews{
+  _id:string;
+  reviewName:string;
+  reviewEmail:string;
+  reviewCountry:string;
+  reviewText:string;
+}
+
+interface review{
+  reviewName:string;
+  reviewEmail:string;
+  reviewCountry:string;
+  reviewText:string;
+}
+
+const submitReview=gql`
+mutation submitReview($inputReview:inputReviewData!)
+{
+  submitReview(inputReview:$inputReview){
+    _id
+  }
+}
+
+`;
+
+const deleteReview =gql`
+
+mutation deleteReview($reviewId:String!){
+  deleteReview(reviewId:$reviewId)
+}
+`;
+
+const getReviews = gql`
+
+query getReviews($reviewId:String!)
+{
+   getReviews(reviewId:$reviewId){
+    review{
+      reviewName
+      reviewEmail
+      reviewCountry
+      reviewText
+    }
+  }
+}
+`;
 
 const Review = gql`
 mutation getUserById($Id:String!){
@@ -25,7 +85,14 @@ mutation getUserById($Id:String!){
 })
 export class ReviewComponent implements OnInit {
 
-  constructor(private apollo :Apollo,private router:Router) { }
+  constructor(private apollo : Apollo,private router:Router,private route: ActivatedRoute) {
+    this.router.events.subscribe(res=>{
+      if(res instanceof NavigationEnd){
+        this.getReviewsOnLoad();
+      }
+
+    })
+  }
 
   reviewForm = new FormGroup({
     reviewName : new FormControl('',[Validators.required]),
@@ -34,11 +101,67 @@ export class ReviewComponent implements OnInit {
     reviewText : new FormControl('',[Validators.required])
   });
 
+  allReviews : reviews[]=[];
+  private querySubscription!: Subscription;
   ngOnInit(): void {
+    this.getReviewsOnLoad();
   }
 
-  review(){
-      this.router.navigateByUrl("/home")
+  reviewName:string="";
+  reviewEmail:string="";
+  reviewCountry:string="";
+  reviewText:string="";
+
+  getReviewsOnLoad(){
+    this.querySubscription = this.apollo.watchQuery<reviewResponse>({
+      query:getReviews,
+      variables:{
+        reviewid : localStorage.getItem("accesstokenid")
+      }
+    }).valueChanges.subscribe((data)=>{
+      this.allReviews = data.data.getReview.reviews
+      
+    })
+  }
+
+  deleteReview(id:any,index:any){
+    this.apollo.mutate({
+
+      mutation:deleteReview,
+
+      variables:{
+        reviewId:id
+      },
+
+    }).subscribe(res=>{
+      
+      let currentReviews = [...this.allReviews];
+      currentReviews.splice(index,1);
+      this.allReviews = currentReviews;
+    })
+  }
+
+  submitReview(){
+    var review  = {
+      userId : localStorage.getItem("accesstokenid"),
+      reviewName : this.reviewName,
+      reviewEmail : this.reviewEmail,
+      reviewCountry : this.reviewCountry,
+      reviewText : this.reviewText,
+    }
+
+    this.apollo.mutate<any>({
+      mutation:submitReview,
+      variables:{
+        inputReview:review,
+      }
+    }).subscribe(res=>{
+      
+      if(res.data.submitReview._id){
+        location.href = '/review';
+      }
+
+    })
   }
 
 }
